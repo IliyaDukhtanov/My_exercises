73803d882cae06d2faa98f7d9cd2da4bc48384d8
Iliya <ilusann@inbox.ru>
2022-08-23 13:18:17 +0300

    Добавлено программа нахождения серднгего арифметического на C#

---
+++ Tasks_from_first_course_coding_meeting001/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 3. Написать программу нахождения среднего арифметического из 4-х чисел.
+
+Console.Write("Введите число A: ");
+double a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+double b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число C: ");
+double c = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число D: ");
+double d = Convert.ToInt32(Console.ReadLine());
+double avg = (a+b+c+d)/4;
+Console.WriteLine("Среднее арифметические введённых чисел = " + avg);
+
+// *Сделать программу для ввода N чисел и вычисления среднего арифметического
