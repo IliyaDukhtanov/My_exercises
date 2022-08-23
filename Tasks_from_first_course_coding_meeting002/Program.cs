d6a56a6d8cd154743aca4a0a6d294b2bb76d4d9e
Iliya <ilusann@inbox.ru>
2022-08-23 13:20:17 +0300

    Добавлена программа нахождения факториала N!

---
+++ Tasks_from_first_course_coding_meeting002/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 6. Написать программу нахождения факториала N
+
+Console.Write("Введите число N, для которого нужно вычислить факториал: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int fact = 1;
+for (int i = 1; i <= n; i++)
+{
+    fact = fact * i;
+}
+Console.WriteLine(n +"! = " + fact);
