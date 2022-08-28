a4c40a07e18e1c7beaa3849a9b9453b14fd383fd
Iliya <ilusann@inbox.ru>
2022-08-28 15:04:46 +0300

    Добавлена программа печати натуральных чисел с использованием рекурсии

---
+++ Recursion_001/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу на C # Sharp для печати первого n натурального числа с использованием рекурсии. 
+// Тестовые данные :
+// Сколько номеров печатать: 10
+// Ожидаемый результат : 1 2 3 4 5 6 7 8 9 10
+
+class Recursion 
+{
+    static int printNatural(int num, int count)
+    {
+        if (count < 1) 
+        {
+            return num;
+        }   
+        count--;
+        Console.Write(" {0} ", num);
+        return printNatural(num + 1, count);
+    }
+    static void Main()
+    {
+        Console.Write("\n\n Рекурсия : Печать первых n натуральных чисел :\n");
+	    Console.Write("---------------------------------------------------\n");
+	    Console.Write(" Сколько чисел печатать : ");
+	    int num= Convert.ToInt32(Console.ReadLine());
+	    // Вызов рекурсивного метода с двумя параметрами.	
+	    printNatural(1, num);
+	    Console.Write("\n\n");
+    } 
+}  
