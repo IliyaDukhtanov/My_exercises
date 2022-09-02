bc1eec3e54ecb6f642cbd4e87b71509f43860988
Iliya <ilusann@inbox.ru>
2022-09-02 11:04:15 +0300

    Добавлено перове задание Вывод приветствия.

---
+++ Base_exercises_C#_001/Program.cs
@@ -0,0 +1,5 @@
+﻿// Напишите программу на C # Sharp, чтобы напечатать Hello и ваше имя в отдельной строке.
+using System; 
+Console.Write("Введите Ваше имя: ");
+string name = Console.ReadLine();
+Console.WriteLine($"Здравствуйте,  {name}!");
