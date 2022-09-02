2ebc8a57de47ebc28772d8f0c4153b4256c5a8a9
Iliya <ilusann@inbox.ru>
2022-09-02 11:18:49 +0300

    Добавлено третье задание. Деление чисел

---
+++ Base_exercises_C#_003/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу на C # Sharp, чтобы напечатать результат деления двух чисел.
+
+//int a = 10; 
+//int b = 5;
+//Console.WriteLine($"Результат деления {a} / {b} равен {a/b}");
+
+
+// вариант кода с исмользованием метода
+public class Exercise3
+{
+  public static void Main()
+  { 
+    System.Console.WriteLine(36/6);
+  }
+}
