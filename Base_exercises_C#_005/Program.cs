9ac0c10c2c7ba9adae14df1525f7d50bedc670f8
Iliya <ilusann@inbox.ru>
2022-09-02 11:57:16 +0300

    Добавлено пятое задание. Замена переменных местами.

---
+++ Base_exercises_C#_005/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу на C # Sharp, чтобы поменять местами два числа. 
+
+Console.Write("Введите первое число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите первое число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine($"После обмена число A {a}, число B {b}");
+ChangeNumbers(a, b);
+
+void ChangeNumbers(int a, int b)
+{
+    int temp = a;
+    a = b; 
+    b = temp;
+    Console.WriteLine($"После обмена: число A: {a}, число B: {b}");
+}
