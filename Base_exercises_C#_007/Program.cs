0df7bc7b549c1f5d83a0ff652ba5f443bf1efd26
Iliya <ilusann@inbox.ru>
2022-09-09 11:16:06 +0300

    Добавлена программа вывода на экран арифметических действий

---
+++ Base_exercises_C#_007/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу на C # Sharp, чтобы выводить на экран 
+//вывод сложения, вычитания, умножения и деления двух чисел, которые будут введены пользователем.
+
+static void Calculation(int a, int b)
+{
+    Console.WriteLine($"Результат сложения чисел {a} + {b} = {a+b}");
+    Console.WriteLine($"Результат вычитания чисел {a} - {b} = {a-b}");
+    Console.WriteLine($"Результат умножения чисел {a} * {b} = {a*b}");
+    Console.WriteLine($"Результат деления чисел {a} / {b} = {a/b}");
+    Console.WriteLine($"Остаток от деления {a} / {b} = {a%b}");
+}
+
+Console.Write("Введите первое число A: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите первое число B: ");
+int b = int.Parse(Console.ReadLine());
+
+Calculation(a, b);
