8483b85990b5e8b327b08745e2cb38a6f1b06a75
Iliya <ilusann@inbox.ru>
2022-09-09 11:03:25 +0300

    Добавлена программа умножения трёх чисел

---
+++ Base_exercises_C#_006/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу на C # Sharp для вывода результата умножения трех чисел, 
+// которые будут введены пользователем.
+
+static void NumbersMultiplication(int a, int b, int c)
+{
+    Console.WriteLine($"Результат умножения чисел {a} * {b} * {c} = {a*b*c}");
+}
+
+Console.Write("Введите первое число A: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите первое число B: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите первое число C: ");
+int c = int.Parse(Console.ReadLine());
+
+NumbersMultiplication(a, b, c);
+//Console.WriteLine($"Результат умножения чисел {a} * {b} * {c} = {a*b*c}");
