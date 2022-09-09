d4c41fd84e96db58c01d2829fa2c222bf39ab145
Iliya <ilusann@inbox.ru>
2022-09-09 11:25:48 +0300

    Бобавлена программа для вывода таблицы умножения введённого числа

---
+++ Base_exercises_C#_008/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу на C # Sharp, которая принимает число в качестве входных данных, и распечатайте таблицу умножения.
+
+static void MultiplicationTable(int a)
+{
+    Console.WriteLine($"Таблица умножения для числа {a}: ");
+    for (int i = 0; i <=10; i++)
+    {
+        Console.WriteLine($"{a} * {i} = {a*i}");;
+    }
+}
+
+Console.Write("Введите число A: ");
+int a = int.Parse(Console.ReadLine());
+
+MultiplicationTable(a);
