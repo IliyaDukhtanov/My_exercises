40f7435ab2b5a36af91db39fd0cc4464fefdcadc
Iliya <ilusann@inbox.ru>
2022-09-26 12:17:38 +0300

    Добавлена задача 42 перевод вимволов в верхний/нижний регистр

---
+++ Base_exercises_C#_042/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу на C # для создания новой строки, где первые 4 символа будут в нижнем регистре. 
+// Если строка меньше 4 символов, тогда сделайте всю строку в верхнем регистре.
+
+Console.WriteLine("Введите строку символов ");
+string symbols = Console.ReadLine();
+if (symbols.Length < 4) Console.WriteLine(symbols.ToUpper());
+else Console.WriteLine(symbols.Substring(0, 4).ToLower() + symbols.Substring(4, symbols.Length-4));
