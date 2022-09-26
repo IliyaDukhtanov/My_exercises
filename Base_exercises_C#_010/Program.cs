4906714f52f5bc635d069176dd1c5540932f3fdc
Iliya <ilusann@inbox.ru>
2022-09-26 11:11:00 +0300

    Добавлена 10 задача. Конвертер Цельсия

---
+++ Base_exercises_C#_010/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу на C # для перевода из градусов Цельсия в Кельвины и Фаренгейты
+
+
+void CelsiusConvert(double celsius)
+{
+    Console.WriteLine($"Температура в Кельвинах равна {celsius + 273.15}");
+    Console.WriteLine($"Температура по Фаренгейту равна {celsius *  1.8 + 32}");
+}
+
+Console.Write("Введите температуру в градусай Цельсия: ");
+double celsius = Convert.ToDouble(Console.ReadLine());
+CelsiusConvert(celsius);
