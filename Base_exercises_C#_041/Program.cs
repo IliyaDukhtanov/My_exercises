f625bbe0e15c3f8f38cc7571c8691713f3b21a2c
Iliya <ilusann@inbox.ru>
2022-09-26 11:50:51 +0300

    Добавлена задача 41 поиск символов строке

---
+++ Base_exercises_C#_041/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу на C #, чтобы проверить, содержит ли данная строка символ «w» от 1 до 3 раз.
+
+void StringCheck(string symbols)
+{
+int count = 0;
+for (int i = 0; i < symbols.Length; i++)
+{
+    if (symbols[i] == 'w') count++;
+}
+if (count <= 3 && count >= 1) Console.WriteLine("Символ 'w' встречается в ведённой строке от 1 до 3 раз");;
+}
+
+Console.WriteLine("Введите строку: ");
+string symbols = Console.ReadLine();
+StringCheck(symbols);
