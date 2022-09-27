f84d42e0b2bbcfdb663ae9f7a6c22b70ab2c457d
Iliya <ilusann@inbox.ru>
2022-09-27 11:36:01 +0300

    Добавлено упражнение 44 Печать нечетных символов строки

---
+++ Base_exercises_C#_044/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу на C # для создания новой строки каждого другого символа (нечетная позиция) из первой позиции данной строки.
+using System;
+
+//Вариант 1
+// static void OutputOdd(string input)
+// {
+//     for (int i = 0; i < input.Length; i++)
+//     {
+//         Console.Write(input[i]);
+//         i++;
+//     }
+// }
+
+//Вариант 2
+Console.WriteLine("Введите строку: ");
+string input = Console.ReadLine();
+var output = string.Empty;
+for (int i = 0; i < input.Length; i++)
+{
+    if (i % 2 == 0) output += input[i];
+}
+Console.WriteLine(output);
+
+
